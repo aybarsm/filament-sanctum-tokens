bb3138ccb5c2ab7e08c9ca15e20f49dc306a40da
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2026-04-06 11:44:04 +0400

    ServiceProvider, auto discovery

---
+++ src/FilamentSanctumTokensServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aybarsm\Filament\SanctumTokens;
+
+use Illuminate\Support\ServiceProvider;
+
+final class FilamentSanctumTokensServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
